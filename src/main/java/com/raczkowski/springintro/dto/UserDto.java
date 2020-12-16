d886fa5fc11f3d4d60c6244c8151e8c31e05feca
praczkowski <p.raczkowski@ocado.com>
2020-12-16 01:21:11 +0100

    Initial commit

--- src/main/java/com/raczkowski/springintro/dto/UserDto.java
+++ src/main/java/com/raczkowski/springintro/dto/UserDto.java
@@ -1,28 +1,11 @@
 package com.raczkowski.springintro.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class UserDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String username;
     private String password;
 
     public UserDto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
--- src/main/java/com/raczkowski/springintro/entity/User.java
+++ src/main/java/com/raczkowski/springintro/entity/User.java
@@ -1,8 +1,15 @@
 package com.raczkowski.springintro.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "users")
 public class User {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String username;
 
@@ -14,6 +21,14 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUsername() {
