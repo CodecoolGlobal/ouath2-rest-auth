ea060bd5bfcb01e85e8d7e0b8feff394e56c6b03
praczkowski <p.raczkowski@ocado.com>
2020-12-20 10:41:29 +0100

    curl updated

--- src/main/resources/curl.sh
+++ src/main/resources/curl.sh
@@ -1,4 +1,4 @@
-curl -i http://localhost:8080/success
+curl -i "http://localhost:8080/api/v1/customers?sortBy=DATE"
 
 curl -i --header "Content-Type: application/json" \
        --request POST \
@@ -6,3 +6,5 @@ curl -i --header "Content-Type: application/json" \
        http://localhost:8080/login
 
 curl -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkdXBhIiwiaWF0IjoxNjA4MzMyNjY5LCJleHAiOjE2MDg0MTkwNjl9.LkOT2hiJ3iEMe91-hamDtf3Ug72JualjdRApih4YiYq1HN8xfOZhh-hiFkXET63bznM3Fw_euppC94HLrIW8Ow" http://localhost:8080/api/v1/customers?sortBy=DATE
+
+curl -i --cookie "JSESSIONID=918D5C98D6200A4B19A347D4580F1581" "http://localhost:8080/api/v1/customers?sortBy=DATE"
